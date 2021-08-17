1a100bea3db3285e1d5652ac90f6c60a7df32f4b
Davidhct <davidhct1@gmail.com>
2021-08-17 16:41:42 +0300

    update navigation with router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { sections } from "./directory.data";
 
-import { MenuItem } from "../menu-item/menu-item.component";
+import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
 
-export class Directory extends React.Component {
+class Directory extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -16,10 +16,11 @@ export class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
   }
 }
+export default Directory;
