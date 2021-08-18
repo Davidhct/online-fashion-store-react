95b3d08b63c7cf081e5f5faf45e106ae8a34b2fe
Davidhct <davidhct1@gmail.com>
2021-08-18 13:26:51 +0300

    add components sign in and out, form input and custom button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignOut from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       <Switch>
         <Route exact path="/online-fashion-store-react" component={HomePage} />
         <Route path="/online-fashion-store-react/shop" component={ShopPage} />
+        <Route
+          path="/online-fashion-store-react/signin"
+          component={SignInAndSignOut}
+        />
       </Switch>
     </div>
   );
